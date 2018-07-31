e1d89540fa6476db94615ae54d73028a1f9b2d39
nikolamilojkovic123 <milojkovicnikolaa@gmail.com>
2018-07-31 15:07:59 +0200

    Html za prijavu

--- projekat/src/app/komponente/login/login.component.ts
+++ projekat/src/app/komponente/login/login.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  email:string;
+  lozinka:string;
+
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  PrijaviSe()
+  {
+    console.log(this.email+" "+this.lozinka);
   }
 
 }
